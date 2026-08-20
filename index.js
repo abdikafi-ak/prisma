1f6cbcbd961bd46c93d1af6935fbc4e77f914bf2
kaaficade2005 <kaaficade7878@gmail.com>
2026-08-20 11:27:23 +0300

    feat(prisma-upgrade-v7): add comprehensive documentation for driver adapters, environment variables, ESM support, and configuration changes in Prisma v7
    docs(prisma-upgrade-v7): create references for removed features, schema changes, and migration guides to assist users transitioning from v6 to v7
    
    chore(prisma-upgrade-v7): initialize prisma.config.ts and schema.prisma files with basic configurations for database setup
    
    fix(prisma-upgrade-v7): update skills-lock.json to include new references and ensure compatibility with Prisma v7

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
 import express from "express";
-import { PrismaClient } from "./generated/prisma/client.js";
-
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-    try {
-        const users = await prisma.user.findMany();
 
-        if (users.length === 0) {
-            return res.status(200).json({
-                status: false,
-                message: "No data in the table",
-                data: []
-            });
-        }
-
-        return res.status(200).json({
-            status: true,
-            data: users
-        });
-
-    } catch (error) {
-        console.error(error);
-
-        return res.status(500).json({
-            status: false,
-            message: "Internal server error"
-        });
-    }
 });
 
 app.listen(7000, () => {
